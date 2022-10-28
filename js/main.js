1a148ca9c370dee4fe5f26b42cc47b20ebd8d055
何明澳 <hemingao@nanmeis-MacBook-Air.local>
2022-10-28 12:54:31 -0700

    new updates on nav bar.

--- js/main.js
+++ js/main.js
@@ -5,3 +5,18 @@ function openSearch() {
 function closeSearch() {
     document.getElementById("myOverlay").style.display = "none";
   }
+
+
+let icon = document.getElementsByClassName("icon")[0];
+
+icon.addEventListener('click', responsive_control);
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function responsive_control() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
